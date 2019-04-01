fa9f4108abb265b5910ef46c515dd749fa45824a
Jessica Lin <jessicaa.lin@gmail.com>
2019-04-01 12:32:58 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -16,8 +16,8 @@ function whileLoop(num){
 
 function doWhileLoop(num){
   function incrementVariable() {
-  i = i + 1;
-  return i;
+    i = i + 1;
+    return i;
   }
   do{
     console.log("I run once regardless.");
